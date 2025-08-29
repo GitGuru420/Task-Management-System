f9785775e96e35c67a2f91d11ebf91eef3cca903
GitGuru420 <raisulislam1892@gmail.com>
2025-08-29 11:43:02 +0600

    Defined urls and create a functions based views

--- task_management_system/urls.py
+++ task_management_system/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from tasks.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home-view/', home_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("<h1>Welcome to the Task Management System Home Page</h1>")
